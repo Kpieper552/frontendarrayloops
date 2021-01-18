0d00ff645b6cc2f47b6ad6218be3fe20c3c10fdd
Kpieper552 <k.pieper@novi-education.nl>
2021-01-18 15:04:01 +0100

    changes

--- array-loops.js
+++ array-loops.js
@@ -3,6 +3,12 @@
 // --------------------------
 
 const names = ["Henk", "Piet", "Fred", "Joop"];
+const totalnames0 = names[0] + "je, "
+const totalnames1 = names[1] + "je, "
+const totalnames2 = names[2] + "je, "
+const totalnames3 = names[3] + "je"
+const totalnames = totalnames0+totalnames1+totalnames2+totalnames3
+console.log(totalnames)
 
 // Verander de waardes van de array dusdanig dat er -"je" achter iedere naam wordt geplakt
 // Wanneer je de volledige array logt zie je alle nieuwe waardes
@@ -15,6 +21,13 @@ const names = ["Henk", "Piet", "Fred", "Joop"];
 // --------------------------
 
 const numbers = [2, 4, 5, 29, 38];
+const numbers0 = numbers[0] * 3 + ", "
+const numbers1 = numbers[1] * 3 + ", "
+const numbers2 = numbers[2] * 3 + ", "
+const numbers3 = numbers[3] * 3 + ", "
+const numbers4 = numbers[4] * 3
+const numberstotal = numbers0+numbers1+numbers2+numbers3+numbers4
+console.log(numberstotal)
 
 // Verander de waardes van de array dusdanig dat iedere waarde met 3 vermenigvuldigd wordt
 // Wanneer je de volledige array logt zie je alle nieuwe waardes
@@ -26,7 +39,14 @@ const numbers = [2, 4, 5, 29, 38];
 // Opdracht 3
 // --------------------------
 
-const figures = [4, 6, 12, 34, 1];
+const figures = [4, 6, 12, 34, 4];
+const figures0 = numbers[0]+5
+const figures1 = numbers[1]+5
+const figures2 = numbers[2]+10
+const figures3 = numbers[3]+8
+const figures4 = numbers[4]+3
+const figurestotal = figures0+", "+figures1+", "+figures2+", "+figures3+", "+figures4
+console.log(figurestotal)
 
 // Verander de waardes van de array dusdanig dat er 3 bij iedere waarde wordt opgetelt
 // Wanneer je de volledige array logt zie je alle nieuwe waardes
