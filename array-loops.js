c2f592c2c7d148e711d58ac27f8852c3c576a980
Kpieper552 <k.pieper@novi-education.nl>
2021-01-26 14:35:55 +0100

    changesaftRev

--- array-loops.js
+++ array-loops.js
@@ -2,13 +2,13 @@
 // Opdracht 1
 // --------------------------
 
-const names = ["Henk", "Piet", "Fred", "Joop"];
-const totalnames0 = names[0] + "je, "
-const totalnames1 = names[1] + "je, "
-const totalnames2 = names[2] + "je, "
-const totalnames3 = names[3] + "je"
-const totalnames = totalnames0+totalnames1+totalnames2+totalnames3
-console.log(totalnames)
+let names = ["Henk", "Piet", "Fred", "Joop"];
+
+
+for (let i = 0; i < names.length; i++) {
+    names[i] = names[i] + "je";
+}
+console.log(names)
 
 // Verander de waardes van de array dusdanig dat er -"je" achter iedere naam wordt geplakt
 // Wanneer je de volledige array logt zie je alle nieuwe waardes
@@ -21,13 +21,11 @@ console.log(totalnames)
 // --------------------------
 
 const numbers = [2, 4, 5, 29, 38];
-const numbers0 = numbers[0] * 3 + ", "
-const numbers1 = numbers[1] * 3 + ", "
-const numbers2 = numbers[2] * 3 + ", "
-const numbers3 = numbers[3] * 3 + ", "
-const numbers4 = numbers[4] * 3
-const numberstotal = numbers0+numbers1+numbers2+numbers3+numbers4
-console.log(numberstotal)
+
+for (let i = 0; i < numbers.length; i++) {
+    numbers[i] = numbers[i] * 3
+}
+console.log(numbers)
 
 // Verander de waardes van de array dusdanig dat iedere waarde met 3 vermenigvuldigd wordt
 // Wanneer je de volledige array logt zie je alle nieuwe waardes
@@ -39,14 +37,12 @@ console.log(numberstotal)
 // Opdracht 3
 // --------------------------
 
-const figures = [4, 6, 12, 34, 4];
-const figures0 = numbers[0]+5
-const figures1 = numbers[1]+5
-const figures2 = numbers[2]+10
-const figures3 = numbers[3]+8
-const figures4 = numbers[4]+3
-const figurestotal = figures0+", "+figures1+", "+figures2+", "+figures3+", "+figures4
-console.log(figurestotal)
+let figures = [4, 6, 12, 34, 1];
+
+for (let i = 0; i < figures.length; i++) {
+    figures[i] = figures[i] + 3
+}
+console.log(figures)
 
 // Verander de waardes van de array dusdanig dat er 3 bij iedere waarde wordt opgetelt
 // Wanneer je de volledige array logt zie je alle nieuwe waardes
